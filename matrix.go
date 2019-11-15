331cfc2111e0e19c6c23f9273583afb6d479f026
Konstantin8105 <konstantin8105@gmail.com>
2019-11-15 15:47:54 +0300

    add function Put for adding to triplet matrix

--- matrix.go
+++ matrix.go
@@ -1,5 +1,7 @@
 package sl
 
+import "github.com/Konstantin8105/errors"
+
 // MatrixType is type of matrix
 type MatrixType uint8
 
@@ -9,6 +11,18 @@ const (
 	Sltm                   // sparse lower triangular matrix
 	Tm                     // triplet matrix format
 )
+
+func (m MatrixType) String() string {
+	switch m {
+	case Ssm:
+		return "sparse symmetrical matrix"
+	case Sltm:
+		return "sparse lower triangular matrix"
+	case Tm:
+		return "triplet matrix format"
+	}
+	return "not defined matrix type(format)"
+}
 
 // Matrix - sparse symmetrical matrix or sparse lower triangular matrix
 // in compressed-column(CCS) or triplet matrix fotmat.
@@ -57,11 +71,126 @@ const (
 //	ColPos      = [ 0 0 1 1 2 ] # column positions
 //
 // Note:
+//
 //	* all internal struct values are share for adding external features.
 type Matrix struct {
-	Format     MatrixType // matrix type
+	Format     MatrixType // matrix type(format)
 	Size       int        // number of rows and columns
 	Values     []float64  // all non-zero values of matrix
 	RowIndexes []int      // row position for each `values`
 	ColPos     []int      // column positions
+}
+
+// New create a new matrix in triplet format.
+func New(s int) *Matrix {
+	return &Matrix{
+		Format:     Tm,
+		Size:       s,
+		Values:     make([]float64, 0, s),
+		RowIndexes: make([]int, 0, s),
+		ColPos:     make([]int, 0, s),
+	}
+}
+
+// Put add value to matrix in triplet format.
+//
+// Input data:
+//	r - row index
+//	c - column index
+//	x - value
+//
+// function return error if matrix or input data is not valid.
+//
+func (m *Matrix) Put(r, c int, x float64) error {
+	// check input data
+	var et errors.Tree
+	if m == nil {
+		et.Add("Matrix is nil")
+	} else {
+		if m.Format != Tm {
+			et.Add(fmt.Errorf("Matrix type is not Triplet: %s", m.Format))
+		} else {
+			// row check
+			if r < 0 {
+				et.Add(fmt.Errorf("row index is negative: %d", r))
+			}
+			if r >= m.Size {
+				et.Add(fmt.Errorf("row index is outside matrix: %d", r))
+			}
+			// column check
+			if c < 0 {
+				et.Add(fmt.Errorf("column index is negative: %d", c))
+			}
+			if c >= m.Size {
+				et.Add(fmt.Errorf("column index is outside matrix: %d", c))
+			}
+			// index checking
+			if r < c {
+				et.Add(fmt.Errorf("row index %d is less column index %d:"+
+					" not valid add in up at diagonal",
+					r, c))
+			}
+			// value check
+			if math.IsNaN(x) {
+				et.Add(fmt.Errorf("value `x` is Nan value"))
+			}
+			if math.IsInf(x, 0) {
+				et.Add(fmt.Errorf("value `x` is infinity value"))
+			}
+		}
+	}
+	if et.IsError() {
+		et.Name = "function `Entry` error:"
+		return et
+	}
+
+	if x != 0.0 { // ignore zero value for minimaze memory allocation
+		// append new information
+		m.RowIndexes = append(m.RowIndexes, r)
+		m.ColPos = append(m.ColPos, c)
+		m.Values = append(m.Values, x)
+	}
+	return nil
+}
+
+// Transform convert matrix
+//
+// Input data:
+//	mt - matrix type to transformation
+//
+// function return error if matrix or input data is not valid.
+//
+func (m *Matrix) TransformTo(mt MatrixType) error {
+	// check input data
+	var et errors.Tree
+	if m == nil {
+		et.Add("Matrix is nil")
+	} else {
+		switch m {
+		case Ssm, Sltm, Tm:
+		default:
+			et.Add("not valid type of matrix: %s", mt)
+		}
+	}
+	if et.IsError() {
+		et.Name = "function `TransformTo` error:"
+		return et
+	}
+
+	// simple transformation
+	if m.Format == mt {
+		// matrix types(formats) are same
+		return nil
+	}
+	if (m.Format == Ssm && mt == Sltm) || (m.Format == Sltm && mt == Ssm) {
+		// matrix transformation without any changing
+		m.Format = mt
+		return nil
+	}
+
+	// transformation from triplet matrix format
+	// TODO
+
+	m.Format = mt
+	return nil
 }
